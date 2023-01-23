6238abb420ca3089279742867f593cdeeeae1b38
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-01-23 21:48:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './Mongodb/connect.js'
+import dalleRoutes from './routes/dalleRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+    origin: '*',
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
+app.use(express.json({ limit: '50mb' }));
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E!');
+})
+
+const startServer = async () => {
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, (req, res) =>
+            console.log('Backend server is running on http://localhost:8080'));
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();

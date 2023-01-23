99743d4f415c18d2265dc3f895c73bd5d8ec5715
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-01-23 22:36:03 +0530

    Added Server Url origin to CORS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: '*',
+    origin: 'https://adorable-red-coypu.cyclic.app',
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));

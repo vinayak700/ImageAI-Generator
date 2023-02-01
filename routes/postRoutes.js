5abfb257715ea52be794c80a02c2c345a6af73e3
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-02-01 10:51:34 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -11,7 +11,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure,
+    secure:true,
 });
 
 // GET ALL POSTS
